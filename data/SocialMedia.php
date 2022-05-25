6b1d17fbb05ed30537b53835272cf0003af27609
dwiki <dwiki.nuridhuha@gmail.com>
2022-05-25 19:35:30 +0700

    97. Final function

--- data/SocialMedia.php
+++ data/SocialMedia.php
@@ -5,10 +5,18 @@ class SocialMedia
     public string $name;
 }
 
-final class Facebook extends SocialMedia
+class Facebook extends SocialMedia
 {
+    final public function login(string $username, string $password): bool
+    {
+        return true;
+    }
 }
 
 class FakeSocialMedia extends Facebook
 {
+    public function login(string $username, string $password): bool
+    {
+        return false;
+    }
 }
